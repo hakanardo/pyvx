7cd00982c0016c43d266505a54ce30e913af61a9
Hakan Ardo <ardo@maths.lth.se>
2014-10-16 12:19:10 +0200

    version

--- pyvx/__init__.py
+++ pyvx/__init__.py
@@ -3,8 +3,8 @@ from pyvx.types import *
 from pyvx.optimize import *
 from pyvx.pythonic import *
 
-__version__ = '0.1.4'
-__version_info__ = (0, 1, 4)
+__version_info__ = (0, 1, 5)
+__version__ = '.'.join(str(i) for i in __version_info__)
 
 #Graph=CoreGraph
 Graph=OptimizedGraph
