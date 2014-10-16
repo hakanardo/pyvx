453876c17a3a0a4d85e6a4547eaccae7eb30f4c3
Hakan Ardo <ardo@maths.lth.se>
2014-10-16 12:17:10 +0200

    release fixes

--- release.sh
+++ release.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
-rm -rf build dist __pycache__ */__pycache__
-if [ -d build ]; then
-    echo Please clean up
-    exit
-fi
+git tag v`PYTHONPATH=. python -c 'import pyvx; print pyvx.__version__' 2>/dev/null | tail -1` || exit
 git checkout stable || exit
 git pull
 git merge master || exit
-git tag v`PYTHONPATH=. python -c 'import pyvx; print pyvx.__version__' 2>/dev/null | tail -1` || exit
 git push --tags
-python setup.py sdist || exit
-twine upload dist/* || exit
 git co master
