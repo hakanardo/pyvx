437c271676f2f451097f9d84b8df9d5e1be179bf
Hakan Ardo <ardo@maths.lth.se>
2014-10-16 12:18:06 +0200

    release fixes

--- release.sh
+++ release.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ ! -z "`git diff`" ]; then 
+    echo Please commit first
+    exit
+fi
 git tag v`PYTHONPATH=. python -c 'import pyvx; print pyvx.__version__' 2>/dev/null | tail -1` || exit
 git checkout stable || exit
 git pull
