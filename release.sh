f0bc719a1d7b11afe3b330cc7c7467f7993e2f20
Hakan Ardo <ardo@maths.lth.se>
2014-10-16 10:18:01 +0200

    minor

--- release.sh
+++ release.sh
@@ -11,4 +11,5 @@ git merge master || exit
 git tag v`PYTHONPATH=. python -c 'import pyvx; print pyvx.__version__' 2>/dev/null | tail -1` || exit
 git push --tags
 python setup.py sdist || exit
-twine upload dist/*
+twine upload dist/* || exit
+git co master
