cea51837167fdb7309377ab066a141b037dd180e
Hakan Ardo <hakan@debian.org>
2015-08-23 11:37:06 +0200

    QueryContext

--- pyvx/api.py
+++ pyvx/api.py
@@ -4,3 +4,19 @@ class VX(VXTypes):
     def ReleaseContext(self, context):
         c = self._ffi.new('vx_context *', context)
         return self._lib.vxReleaseContext(c)
+
+    def QueryContext(self, context, attribute, c_type, python_type=None):
+        if self._ffi.typeof(c_type).kind != 'array':
+            val = self._ffi.new(c_type + '*')
+            status = self._lib.vxQueryContext(context, attribute, val, self._ffi.sizeof(c_type))
+            val = val[0]
+        else:
+            val = self._ffi.new(c_type)
+            status = self._lib.vxQueryContext(context, attribute, val, self._ffi.sizeof(c_type))
+
+        if python_type is str:
+            val = self._ffi.string(val)
+        elif python_type is not None:
+            val = python_type(val)
+
+        return status, val
--- test/test_vx.py
+++ test/test_vx.py
@@ -3,5 +3,13 @@ from pyvx.default import vx
 class TestVX(object):
     def test_context(self):
         c = vx.CreateContext()
+        s, v = vx.QueryContext(c, vx.CONTEXT_ATTRIBUTE_VENDOR_ID, 'vx_uint16')
+        assert s == vx.SUCCESS
+        assert isinstance(v, int)
+        s, v = vx.QueryContext(c, vx.CONTEXT_ATTRIBUTE_IMPLEMENTATION, 'vx_char[VX_MAX_IMPLEMENTATION_NAME]', str)
+        assert s == vx.SUCCESS
+        assert isinstance(v, str)
+        s, v = vx.QueryContext(c, vx.CONTEXT_ATTRIBUTE_IMMEDIATE_BORDER_MODE, 'vx_border_mode_t')
+        assert s == vx.SUCCESS
         assert vx.ReleaseContext(c) == vx.SUCCESS
 
