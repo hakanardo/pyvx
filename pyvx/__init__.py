fa7c41b701f292d00c827f844ad977928469264c
Hakan Ardo <hakan@debian.org>
2015-09-01 14:28:13 +0200

    some import magic for backwards compatibility

--- pyvx/__init__.py
+++ pyvx/__init__.py
@@ -1,3 +1,18 @@
 __version_info__ = (0, 3, 0)
 __version__ = '.'.join(str(i) for i in __version_info__)
 __backend_version__ = '1.0.1-1'
+
+import sys
+class ImportVX(object):
+
+    def find_module(self, fullname, path=None):
+        if fullname == 'pyvx.vx':
+            return self
+        return None
+
+    def load_module(self, name):
+        from pyvx.default import vx
+        return vx
+
+import sys
+sys.meta_path = [ImportVX()]
