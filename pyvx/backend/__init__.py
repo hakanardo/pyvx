7f5df3c086c8cd77b0ba0826036a4e2002f5ac9f
Hakan Ardo <hakan@debian.org>
2015-09-23 08:31:10 +0200

    binary name

--- pyvx/backend/__init__.py
+++ pyvx/backend/__init__.py
@@ -1,11 +1,11 @@
-import pyvx
+import pyvx, sys
 
 if isinstance(pyvx._default_backend_name, str):
     try:
         exec("from pyvx.backend.%s import lib, ffi" % pyvx._default_backend_name)
     except ImportError:
         raise ImportError("No default backend found. Please build one using:\n\n" +
-                          "    python -mpyvx.build_cbackend --default name /path/to/openvx/install\n" +
+                          "    %s -mpyvx.build_cbackend --default name /path/to/openvx/install\n" % (sys.executable) +
                           "")
 else:
     lib = pyvx._default_backend_name.lib
